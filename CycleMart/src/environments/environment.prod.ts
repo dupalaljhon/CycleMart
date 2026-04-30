b039421397b5be5fd727a731da8273d18d25d373
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2026-04-30 12:20:47 +0800

    for deployement

--- CycleMart/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-	production: true,
-	apiBaseUrl: 'https://api.cyclemart.shop/api',
-	apiUploadsBaseUrl: 'https://api.cyclemart.shop/uploads/'
-};
--- CycleMart/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-	production: false,
-	apiBaseUrl: 'http://localhost:3001/api',
-	apiUploadsBaseUrl: 'http://localhost:3001/uploads/'
-};
