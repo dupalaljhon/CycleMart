f9f8709d1075ba7b58d24a287a177cfcc1ae1369
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2026-05-11 16:29:25 +0800

    updateeeeeeeeeeeeee

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
-	// production: true,
-	// apiBaseUrl: 'http://localhost:3001/api',
-	// apiUploadsBaseUrl: 'http://localhost:3001/uploads/',
-	// socketUrl: 'http://localhost:3001'
-
-	
-		production: false,
-	apiBaseUrl: 'http://159.65.15.15:3000/api',
-	apiUploadsBaseUrl: 'http://159.65.15.15:3000/uploads/',
-	socketUrl: 'http://159.65.15.15:3000'
+	production: true,
+	apiBaseUrl: '/api',
+	apiUploadsBaseUrl: '/uploads/',
+	socketUrl: ''
 };
