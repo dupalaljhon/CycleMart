7366de60146a0257040f4efbb7a121272fb7a955
ALJHONDUPAL <202011035@gordoncollege.edu.ph>
2026-05-14 14:15:38 +0800

    updat emjs

--- CycleMart-api-node/src/utils/email.js
+++ CycleMart-api-node/src/utils/email.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: Number(process.env.SMTP_PORT || 465),
-  secure: true,
+  secure: process.env.SMTP_SECURE === 'true',  // true for port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
